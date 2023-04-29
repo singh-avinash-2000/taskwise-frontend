6ebfeb4684d082637bacb102a6603fbf0d089ed1
Ritik Mishra <ritikmishra7@gmail.com>
2023-04-29 12:02:18 +0530

    refactored

--- src/components/ui/Auth/OnlyIfNotLoggedIn.jsx
+++ src/components/ui/Auth/OnlyIfNotLoggedIn.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 const OnlyIfNotLoggedIn = () =>
 {
 	const navigate = useNavigate();
-	const location = useLocation();
+	// const location = useLocation();/
 
-	const fetchRefreshToken = async () =>
-	{
-		const response = await axios.create({
-			withCredentials: true
-		}).get(process.env.REACT_APP_BASE_URL + "/auth/refresh");
+	// const fetchRefreshToken = async () =>
+	// {
+	// 	const response = await axios.create({
+	// 		withCredentials: true
+	// 	}).get(process.env.REACT_APP_BASE_URL + "/auth/refresh");
 
-		let newToken = response.data.result.accessToken;
-		localStorage.setItem("accessToken", newToken);
+	// 	let newToken = response.data.result.accessToken;
+	// 	localStorage.setItem("accessToken", newToken);
 
-		navigate("/");
-	};
+	// 	navigate("/");
+	// };
 
 	useEffect(() =>
 	{
 		let token = localStorage.getItem("accessToken");
-		const loggedOut = location.state?.loggedOut || false;
+		// const loggedOut = location.state?.loggedOut || false;
 
 		if (token)
 		{
--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -54,7 +54,6 @@ const Dashboard = () =>
 	{
 		fetchProjects();
 		setActiveProjectName("");
-		fetchProjects();
 	}, []);
 
 	return (

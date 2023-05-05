4c02ff42ec491fd70b4b971116ad927c18df40ad
Avinash Singh <workwith.avinashsingh@gmail.com>
2023-05-05 22:29:41 +0530

    merge conflict

--- src/components/ui/Notification/Notification.jsx
+++ src/components/ui/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import "./Notification.css";
 import { Avatar, message } from "antd";
 import { List } from "react-virtualized";
